5c11371cac5a4716deb59a3615054ce3fe51cbaa
byigitt <137426514+byigitt@users.noreply.github.com>
2025-01-11 16:08:23 +0300

    feat(users): add DTOs for creating and updating user profiles
    - Implement CreateUserDto with validation for email, password, first name, last name, and optional roles.
    - Introduce UpdateUserDto extending CreateUserDto, omitting the password field for partial updates.
    
    These DTOs facilitate user management by ensuring data integrity and validation during user creation and updates.

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  IsOptional,
+  IsEnum,
+} from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Role } from '../../../common/decorators/roles.decorator';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'john.doe@example.com' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'StrongP@ssw0rd' })
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @ApiProperty({ example: 'John' })
+  @IsString()
+  firstName: string;
+
+  @ApiProperty({ example: 'Doe' })
+  @IsString()
+  lastName: string;
+
+  @ApiPropertyOptional({ enum: Role, isArray: true, example: [Role.USER] })
+  @IsOptional()
+  @IsEnum(Role, { each: true })
+  roles?: Role[];
+}
